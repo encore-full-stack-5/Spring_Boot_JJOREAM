0ebcd85a4655ab2e14c9b289696adca337c99058
jungryuel <zldrktduf0927@naver.com>
2024-04-17 17:17:34 +0900

    myPage

---
+++ joream/src/pages/Wishlist.jsx
@@ -0,0 +1,16 @@
+// import { login, register } from "../api/auth";
+import "../Wishlist.css";
+const Wishlist = () => {
+  return (
+    <>
+      <div className="navbar">aaa</div>
+      <div className="main">
+        <div className="main-right">
+          aa
+          <a>마이페이지</a>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Wishlist;
