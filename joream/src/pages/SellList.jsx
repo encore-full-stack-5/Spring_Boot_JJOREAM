f4b3e7deede675e4876d34a9522fd5555f3e37f2
jungryuel <zldrktduf0927@naver.com>
2024-04-19 19:53:00 +0900

    product sell & buy

--- joream/src/pages/SellList.jsx
+++ joream/src/pages/SellList.jsx
@@ -67,6 +67,104 @@ const SellList = () => {
               </Nav.Link>
             </Nav.Item>
           </Nav>
+          <div
+            style={{
+              marginTop: "150px",
+              width: "100%",
+              height: "80px",
+              display: "flex",
+              flexDirection: "row",
+              border: "2px solid black",
+            }}
+          >
+            <div style={{ display: "flex", width: "40%" }}>
+              <div
+                style={{
+                  position: "relative",
+                  width: "80px",
+                  height: "100%",
+                  textAlign: "center",
+                }}
+              >
+                이미지
+              </div>
+              <div
+                style={{
+                  alignSelf: "center",
+                  marginLeft: "16px",
+                  width: "65%",
+                  height: "80px",
+                }}
+              >
+                <div>나이키 에어포스</div>
+              </div>
+            </div>
+            <div
+              style={{
+                textAlign: "end",
+                width: "40%",
+              }}
+            >
+              구매가격
+            </div>
+            <div
+              style={{
+                textAlign: "end",
+                width: "20%",
+              }}
+            >
+              구매일
+            </div>
+          </div>
+          <div
+            style={{
+              marginTop: "10px",
+              width: "100%",
+              height: "80px",
+              display: "flex",
+              flexDirection: "row",
+              border: "2px solid black",
+            }}
+          >
+            <div style={{ display: "flex", width: "40%" }}>
+              <div
+                style={{
+                  position: "relative",
+                  width: "80px",
+                  height: "100%",
+                  textAlign: "center",
+                }}
+              >
+                이미지
+              </div>
+              <div
+                style={{
+                  alignSelf: "center",
+                  marginLeft: "16px",
+                  width: "65%",
+                  height: "80px",
+                }}
+              >
+                <div>나이키 에어포스</div>
+              </div>
+            </div>
+            <div
+              style={{
+                textAlign: "end",
+                width: "40%",
+              }}
+            >
+              구매가격
+            </div>
+            <div
+              style={{
+                textAlign: "end",
+                width: "20%",
+              }}
+            >
+              구매일
+            </div>
+          </div>
         </div>
       </div>
     </>
